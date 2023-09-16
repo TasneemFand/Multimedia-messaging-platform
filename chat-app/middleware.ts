77734afad88dc70cbee2790743beeefb571fd66f
Tasneem Fandakli <tasnim.tepe@gmail.com>
2023-09-16 16:53:56 +0300

    set up middleware file to protect the routes

---
+++ chat-app/middleware.ts
@@ -0,0 +1,15 @@
+/**
+ * decide which pages are public and which need to hide behind authentication.
+ * by creating a middleware.ts file at the root folder.
+ */
+import { authMiddleware } from "@clerk/nextjs";
+ 
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.
+// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
+export default authMiddleware({});
+ 
+export const config = {
+      matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};
+ 
