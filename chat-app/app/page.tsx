394cac8295d63966eca8181b99c32f00cfb33c54
Tasneem Fandakli <tasnim.tepe@gmail.com>
2023-09-16 15:54:44 +0300

    use cn of utils in shadcn

--- chat-app/app/page.tsx
+++ chat-app/app/page.tsx
@@ -1,12 +1,16 @@
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 
 export default function Home() {
+	const state = true;
   return (
 	<div className="flex flex-col">
 		<p className='text-3xl font-bold text-indigo-300'>
 			Hello chat app
 		</p>
-		<Button>
+		<Button className={
+			cn('bg-cyan-50', state && 'bg-slate-600')			
+		}>
 			click me
 		</Button>
 	</div>
