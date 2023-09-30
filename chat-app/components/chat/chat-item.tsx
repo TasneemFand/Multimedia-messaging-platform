86f38fcd62c4cf4920d3d76ff7b6c2c3b58a5d84
Tasneem Fandakli <tasnimtepe@gmail.com>
2023-09-30 20:05:33 +0300

    handle member click in chat message

--- chat-app/components/chat/chat-item.tsx
+++ chat-app/components/chat/chat-item.tsx
@@ -22,6 +22,7 @@ import {
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { useModal } from "@/hooks/use-modal-store";
+import { useParams, useRouter } from "next/navigation";
 
 interface ChatItemProps {
 	id: string;
@@ -84,6 +85,16 @@ export const ChatItem = ({
 
 	const isLoading = form.formState.isSubmitting;
 
+	const router = useRouter();
+	const params = useParams();
+
+	const onMemberClick = () => {
+		if (member.id === currentMember.id) {
+		  return;
+		}
+	  
+		router.push(`/servers/${params?.serverId}/conversations/${member.id}`);
+	}
 	const onSubmit = async (values: z.infer<typeof formSchema>) => {
 		try {
 			const url = qs.stringifyUrl({
@@ -122,13 +133,13 @@ export const ChatItem = ({
 	return (
 		<div className="relative group flex items-center hover:bg-black/5 p-4 transition w-full">
 			<div className="group flex gap-x-2 items-start w-full">
-				<div className="cursor-pointer hover:drop-shadow-md transition">
-				<UserAvatar src={member.profile.imageUrl} />
+				<div onClick={onMemberClick} className="cursor-pointer hover:drop-shadow-md transition">
+					<UserAvatar src={member.profile.imageUrl} />
 				</div>
 				<div className="flex flex-col w-full">
 					<div className="flex items-center gap-x-2">
 						<div className="flex items-center">
-							<p className="font-semibold text-sm hover:underline cursor-pointer">
+							<p onClick={onMemberClick} className="font-semibold text-sm hover:underline cursor-pointer">
 								{member.profile.name}
 							</p>
 							<ActionTooltip label={member.role}>
